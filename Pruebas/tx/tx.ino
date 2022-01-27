895daf1762e025baf41874fa5c64ce02e508d188
isabelkl19 <59041222+isabelkl19@users.noreply.github.com>
2022-01-27 14:18:00 -0500

    Update tx.ino

--- Pruebas/tx/tx.ino
+++ Pruebas/tx/tx.ino
@@ -8,12 +8,16 @@
 
 #include <SoftwareSerial.h>
 SoftwareSerial HC12(2, 3); // HC-12 TX Pin, HC-12 RX Pin
+int i = 0;
 
 void setup() {
   Serial.begin(9600);             // Serial port to computer
   HC12.begin(9600);               // Serial port to HC12
 }
 void loop() {
-  float val = 19;
-  HC12.write(val);
+  for (i; i <= 10; i++ ) {
+    HC12.write(19);
+    delay(3000);
+  }
+  i = 15;
 }
