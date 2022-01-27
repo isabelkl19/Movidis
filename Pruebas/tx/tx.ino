c461c9abb9ff8a8f675a2e298dc6bc65354d7a57
isabelkl19 <59041222+isabelkl19@users.noreply.github.com>
2022-01-27 13:01:48 -0500

    sin problemas

--- Pruebas/tx/tx.ino
+++ Pruebas/tx/tx.ino
@@ -14,14 +14,6 @@ void setup() {
   HC12.begin(9600);               // Serial port to HC12
 }
 void loop() {
-  String vw_send;
-  const char msgChar[] =  "B";
-  for (int i = 0; i < 100; i++)
-  {
-    HC12.write((uint8_t *)msgChar, strlen(msgChar));
-    //vw_send = ((uint8_t *)msgChar, strlen(msgChar));
-    delay (300);
-  }
-  //HC12.write(vw_send);      // Send that data to HC-12
-  delay (15000);
+  float val = 19;
+  HC12.write(val);
 }
